180a342596049bc962fcdfd7faf56b6d2c7c2395
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-11 06:53:36 -0600

    trial: console cors.

--- server/index.js
+++ server/index.js
@@ -2,8 +2,4 @@ const app = require("./server");
 const logger = require("./config/logger");
 const port = process.env.PORT;
 
-app.listen(port, () =>
-  logger.debug(
-    `Serving Bundles of Sage @: ${port}, allowing: ${process.env.FRONTEND_URL}.`
-  )
-);
+app.listen(port, () => logger.debug(`Serving Bundles of Sage @: ${port}`));
