c960f3e68b11c7b46203a7d7d8b085fbea3c06be
Ryan Brockhoff <brockhoffrw@gmail.com>
2019-05-11 06:50:29 -0600

    attempt: checking url.

--- server/index.js
+++ server/index.js
@@ -2,4 +2,8 @@ const app = require("./server");
 const logger = require("./config/logger");
 const port = process.env.PORT;
 
-app.listen(port, () => logger.debug(`Serving Bundles of Sage @: ${port}`));
+app.listen(port, () =>
+  logger.debug(
+    `Serving Bundles of Sage @: ${port}, allowing: ${process.env.FRONTEND_URL}.`
+  )
+);
